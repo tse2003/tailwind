a9234ae4aea000cfd79c3986723eae5e4625e232
tse2003 <s.tseba0124@gmail.com>
2024-07-14 18:10:14 +0800

    push6

--- components/Experience.js
+++ components/Experience.js
@@ -9,9 +9,42 @@ export function Experience() {
             </p>
         </div>
         <div className="experience-box">
-            <div className="sec1"></div>
-            <div className="sec2"></div>
-            <div className="sec3"></div>
+            <div className="sec1">
+                <p className="text-green-600 font-bold text-xl ml-5 mt-2">upwork</p>
+            </div>
+            <div className="sec2">
+                <p className="font-bold mt-2 ml-5">Sr.Frontend Developer</p>
+                <div className="ml-10 text-gray-600 text-sm mt-2">
+                <ul className="list">
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Ut pretium arcu et massa semper, id fringilla leo semper.</li>
+                        <li>Sed quis justo ac magna.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="sec3">
+                <p  className="text-gray-700 font-bold mt-2 ml-10">Nov 2021 - Present</p>
+            </div>
+        </div>
+        <div className="experience-box">
+            <div className="sec1">
+                <p className="text-green-600 font-bold text-xl ml-5 mt-2">upwork</p>
+            </div>
+            <div className="sec2">
+                <p className="font-bold mt-2 ml-5">Team Lead</p>
+                <div className="ml-10 text-gray-600 text-sm mt-2">
+                <ul className="list">
+                        <li>Sed quis justo ac magna.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Sed quis justo ac magna.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="sec3">
+                <p  className="text-gray-700 font-bold mt-2 ml-10">Jul 2017 - Oct 2021</p>
+            </div>
         </div>
     </div>;
 }
