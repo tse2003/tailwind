6d91b6d96d2109ac13dc0c60dce791738261d2f6
tse2003 <s.tseba0124@gmail.com>
2024-07-16 18:06:57 +0800

    push8

--- components/Work.js
+++ components/Work.js
@@ -1,12 +1,65 @@
+import { AiOutlineExport } from "react-icons/ai";
 export function Work() {
-    return <div>
-        <div className="button">
-            <button class="bg-gray-200 text-black py-2 px-4 rounded-full mt-14">
-                Work
-            </button>
-            <p className="text-sm mt-2 text-gray-600">
-                Some of the noteworthy projects I have built:
-            </p>
+  return (
+    <div>
+      <div className="button">
+        <button class="bg-gray-200 text-black py-2 px-4 rounded-full mt-14">
+          Work
+        </button>
+        <p className="text-sm mt-2 text-gray-600">
+          Some of the noteworthy projects I have built:
+        </p>
+        <div className="work">
+          <div className="work2">
+            <img className="w-80 mt-12 ml-10" src="work.png" />
+          </div>
+          <div className="work3 flex flex-col items-start">
+            <h1 className="ml-5 mt-5 font-bold text-xl">Fiskil</h1>
+                <p className="text-sm ml-5 mt-3 text-gray-600 text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante 
+                    ipsum primis orci et ultrices prosuere cubilia 
+                    curae.
+                </p>
+            <div className="flex ml-5 mt-3 gap-3">
+                <div className="bg-gray-200 w-20 h-7 mt-3 rounded-lg">
+                    <p className="mt-0.5 text-gray-600">React</p>
+                </div>
+                <div className="bg-gray-200 w-20 h-7 mt-3 rounded-lg">
+                    <p className="mt-0.5 text-gray-600">Next.js</p>
+                </div>
+                <div className="bg-gray-200 w-28 h-7 mt-3 rounded-lg">
+                    <p className="mt-0.5 text-gray-600">Typescript</p>
+                </div>
+                <div className="bg-gray-200 w-20 h-7 mt-3 rounded-lg">
+                    <p className="mt-0.5 text-gray-600">Nest.js</p>
+                </div>
+            </div>
+            <div className="flex ml-5 mt-1 gap-3">
+                <div className="bg-gray-200 w-28 h-7 mt-3 rounded-lg">
+                    <p className="mt-0.5 text-gray-600">PostgreSQL</p>
+                </div>
+                <div className="bg-gray-200 w-28 h-7 mt-3 rounded-lg">
+                    <p className="mt-0.5 text-gray-600">Tailwindcss</p>
+                </div>
+                <div className="bg-gray-200 w-20 h-7 mt-3 rounded-lg">
+                    <p className="mt-0.5 text-gray-600">Figma</p>
+                </div>
+                <div className="bg-gray-200 w-20 h-7 mt-3 rounded-lg">
+                    <p className="mt-0.5 text-gray-600">Cypress</p>
+                </div>
+            </div>
+            <div className="flex ml-5 mt-1 gap-3">
+                <div className="bg-gray-200 w-28 h-7 mt-3 rounded-lg">
+                    <p className="mt-0.5 text-gray-600">Storybook</p>
+                </div>
+                <div className="bg-gray-200 w-14 h-7 mt-3 rounded-lg">
+                    <p className="mt-0.5 text-gray-600">Git</p>
+                </div>
+            </div>
+            <AiOutlineExport size={25} className="ml-5 mt-5" />
+          </div>
         </div>
-    </div>;
+      </div>
+    </div>
+  );
 }
